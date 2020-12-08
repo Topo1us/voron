94bb839a252b5dd5ed330f0c2a248c837dc81fa0
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-12-08 21:45:21 +0300

    Update voron.py

--- voron.py
+++ voron.py
@@ -158,7 +158,7 @@ sa  - - afhe
             try:
                 target=input('\nIP: ')
                 time_atack=input('время атаки в секундах: ')
-                print('ls')
+                os.system('ls')
 #                os.system(r'cd &cd voron & cd Impusle & python impulse.py --method HTTP --target '+str(target)+' --time '+str(time_atack)+' --threads 135')
             except:
                 print('Ковальски, у нас ошибка.')
